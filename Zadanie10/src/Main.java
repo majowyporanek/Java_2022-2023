d8306b176bc4b9e8839b6ac6010655e68a80908d
Klaudia <klaudiachwistekklaudia@#gmail.com>
2023-01-06 19:33:48 +0100

    connector done

---
+++ Zadanie10/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String info = "Zaczynamy! Policz ile raz pojawiła się liczba: 2";
+        System.out.println(info.length());
+        System.out.println(info.charAt(info.length()-1));
+        System.out.println(info.charAt(0));
+    }
+}
